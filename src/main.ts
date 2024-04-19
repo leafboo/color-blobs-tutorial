1621d12cb88fabe17d5bc63dd4ecd6122b0dd866
Jan romel <janjancaranguian@gmail.com>
2024-04-19 15:40:51 +0800

    Generate 100 different circles

--- src/main.ts
+++ src/main.ts
@@ -35,31 +35,71 @@ let c = canvas.getContext('2d')!;
 // }
 
 // animation
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) * 10;
-let dy = (Math.random() - 0.5) * 10;
-let radius = 30
+interface Circle {
+  x: number;
+  y: number;
+  dx: number;
+  dy: number;
+  radius: number;
+  draw: () => void;
+  update: () => void;
+  
+}
+ 
+function createCircle(x: number, y: number, dx: number, dy: number, radius: number): Circle {
+  let circle: Circle = {
+    x: x,
+    y: y,
+    dx: dx,
+    dy: dy,
+    radius:radius,
+    draw: function() {
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.strokeStyle = 'blue';
+      c.stroke();
+      this.update()
+    },
+    update: function() {
+      if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+        this.dx = -this.dx
+      }
+      if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+        this.dy = -this.dy
+      }
+    
+      this.x += this.dx
+      this.y += this.dy
+    }
+  };
+  return circle;
+}
+
+
+
+
+let circle: any;
+let circleArray: any = [];
+
+for (let i = 0; i < 100; i++) {
+  let radius = 30;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let y = Math.random() * (innerHeight - radius * 2) + radius;
+  let dx = (Math.random() - 0.5);
+  let dy = (Math.random() - 0.5);
+
+  circleArray.push(createCircle(x, y, dx, dy, radius));
+}
+
+
 
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, innerWidth, innerHeight)
 
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = 'blue';
-  c.stroke()
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx
+  for(let i = 0; i < circleArray.length; i++) {
+    circleArray[i].draw()
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy
-  }
-
-  x += dx
-  y += dy
-
 }
 
 
